49438088b0f3db89c23cf4257ffbc7ab451bee54
Park Sang Min <sm0104224@gmail.com>
2026-03-12 23:48:06 +0900

    feat: enable JPA auditing and scheduling

--- src/main/java/com/c4i/tracking/TargetTrackingApplication.java
+++ src/main/java/com/c4i/tracking/TargetTrackingApplication.java
@@ -2,12 +2,14 @@ package com.c4i.tracking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableScheduling  // 나중에 시뮬레이터용
 public class TargetTrackingApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TargetTrackingApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(TargetTrackingApplication.class, args);
+    }
 }
